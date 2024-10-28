6ee58e21f719d2b0ea831861c17b2d54e7977e6b
vayntail <113714157+vayntail@users.noreply.github.com>
2024-10-28 12:05:29 -0400

    base UI design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi</h1>
+    <div className="app-wrapper">
+      <nav className="navbar"><h1>kanban boards</h1></nav>
+      <main className="board">
+        <h2 className="board-name">board name</h2>
+        <div className="board-columns">
+          <div className="column">
+            <h3>todo</h3>
+            <ul className="cards-list">
+              <li className="card">first</li>
+              <li className="card">first</li>
+              <li className="card">first</li>
+            </ul>
+            <div>
+              <button>add new</button>
+            </div>
+          </div>
+          <div className="column">
+            <h3>todo</h3>
+            <ul className="cards-list">
+              <li className="card">first</li>
+              <li className="card">first</li>
+              <li className="card">first</li>
+            </ul>
+            <div>
+              <button>add new</button>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
